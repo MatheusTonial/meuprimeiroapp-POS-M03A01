77ff8ae120388dce4e1e8846591ec7249b37d5be
Matheus <matheustonial97@gmail.com>
2026-02-07 11:26:44 -0300

    google maps

---
+++ app/src/main/java/com/example/meuprimeiroapp/ItemDetailActivity.kt
@@ -0,0 +1,111 @@
+package com.example.meuprimeiroapp
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.meuprimeiroapp.databinding.ActivityItemDetailBinding
+import com.example.meuprimeiroapp.model.Item
+import com.example.meuprimeiroapp.service.RetrofitClient
+import com.example.meuprimeiroapp.service.safeApiCall
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.example.meuprimeiroapp.service.Result
+import com.example.meuprimeiroapp.ui.loadUrl
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class ItemDetailActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var binding: ActivityItemDetailBinding
+
+    private lateinit var item: Item
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityItemDetailBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        setupView()
+        loadItem()
+        setupGoogleMap()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+    }
+
+    private fun setupView() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
+    private fun loadItem() {
+        val itemId = intent.getStringExtra(ARG_ID) ?: ""
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = safeApiCall { RetrofitClient.apiService.getItem(itemId) }
+
+            withContext(Dispatchers.Main) {
+                when (result) {
+                    is Result.Success -> {
+                        item = result.data
+                        handleSuccess()
+                    }
+                    is Result.Error -> {
+                        Toast.makeText(this@ItemDetailActivity, "Erro ao buscar o item", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupGoogleMap(){
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun handleSuccess(){
+        binding.name.text = item.value.fullName
+        binding.age.text = getString(R.string.item_age, item.value.age)
+        binding.profession.setText(item.value.profession)
+        binding.image.loadUrl(item.value.imageUrl)
+        loadItemInGoogleMap()
+    }
+
+    private fun loadItemInGoogleMap(){
+        item.value.location?.let {
+            binding.googleMapContent.visibility = View.VISIBLE
+            val location = LatLng(it.latitude, it.longitude)
+            mMap.addMarker(MarkerOptions().position(location).title(it.name))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+        }
+
+    }
+
+    companion object {
+        const val ARG_ID = "arg_id"
+
+        fun newIntent(context: Context, itemId: String): Intent {
+            return Intent(context, ItemDetailActivity::class.java).apply {
+                putExtra(ARG_ID, itemId)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.meuprimeiroapp
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.meuprimeiroapp.adapter.ItemAdapter
 import com.example.meuprimeiroapp.databinding.ActivityMainBinding
@@ -9,6 +16,9 @@ import com.example.meuprimeiroapp.model.Item
 import com.example.meuprimeiroapp.service.Result
 import com.example.meuprimeiroapp.service.RetrofitClient
 import com.example.meuprimeiroapp.service.safeApiCall
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,13 +27,16 @@ import kotlinx.coroutines.withContext
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationPermissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+
+        requestLocationPermission()
     }
 
     override fun onResume() {
@@ -38,6 +51,61 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.addCta.setOnClickListener {
 
+        }
+    }
+
+    private fun requestLocationPermission() {
+        //inicializa o FusedLocationPermission
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        //configura o ActivityResultLaucher para solicitar a permissão de localização
+        locationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            isGranted -> if(isGranted){
+                getLastLocation()
+            }
+            else{
+                Toast.makeText(this, "Permissão de localização negada", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        checkLocationPermissionAndRequest()
+    }
+
+    private fun checkLocationPermissionAndRequest() {
+        when {
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED  &&
+                    ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                getLastLocation()
+            }
+            shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION) -> {
+                locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_COARSE_LOCATION) -> {
+                locationPermissionLauncher.launch(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            }
+            else -> {
+                locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    private fun getLastLocation(){
+        //varifica permissao antes
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission()
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnCompleteListener{ task: Task <Location> ->
+            if(task.isSuccessful && task.result != null){
+                val location = task.result
+                val latitude = location.latitude
+                val longitude = location.longitude
+            }
+            else{
+                Toast.makeText(this, "Não foi possível obter a localização", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -59,8 +127,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleOnSuccess(items: List<Item>) {
         binding.recyclerView.adapter = ItemAdapter(items) { item ->
-//            val intent = ItemDetailActivity.newIntent(this, item)
-//            startActivity(intent)
+            val intent = ItemDetailActivity.newIntent(this, item.id)
+            startActivity(intent)
         }
     }
 }
